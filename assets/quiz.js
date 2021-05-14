505296e343f48e8d1a738340d37bf9265cfd07cd
Aislynn Uribe <aislynn.rose@gmail.com>
2021-05-13 21:42:54 -0600

    added additional pages to create the highscore and updated HTML

---
+++ assets/quiz.js
@@ -0,0 +1,29 @@
+var qQuestions = [
+  {
+    Question: "Arrays in JavaScript can be used to store:",
+    answers: ['Numbers and Strings', 'Other Arrays', 'Booleans', 'All of the above'],
+    CorrectAnswer: 'All of the above'
+  },
+  {
+    Question: "Commonly used data types DO NOT include:",
+    answers: ['Strings', 'Booleans', 'Alerts', 'Numbers'],
+    CorrectAnswer: 'Alerts'
+  },
+  {
+    Question: "The condition in an if/else statement is enclosed within:",
+    answers: ['Quotes', 'Curly Brackets', 'Parenthesis', 'Square Brackets'],
+    CorrectAnswer: 'Parenthesis'
+  },   
+  {
+    Question: "String values must be enclosed in _________ when being assigned to variables",
+    answers: ['Commas', 'Curly Brackets', 'Quotes', 'Parenthesis'],
+    CorrectAnswer: 'Quotes'
+  },   
+  {
+    Question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: ['JavaScript', 'Terminal/Bash', 'For loops', 'Console.log'],
+    CorrectAnswer: 'Console.log'
+  },   
+]
+
+let score = '0'
